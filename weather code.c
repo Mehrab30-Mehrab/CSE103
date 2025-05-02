b77953d46288efdf8eaf09ba6a7f918e5a977db6
Mehrab Morshed Marjan <2025-1-60-142@std.ewubd.edu>
2025-05-02 16:20:10 +0600

    Update weather code.c

--- weather code.c
+++ weather code.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int temp;
+
+    // Test data
+    temp = 42;
+
+    if (temp < 0) {
+        printf("Freezing weather\n");
+    } else if (temp >= 0 && temp <= 10) {
+        printf("Very Cold weather\n");
+    } else if (temp > 10 && temp <= 20) {
+        printf("Cold weather\n");
+    } else if (temp > 20 && temp <= 30) {
+        printf("Normal in Temp\n");
+    } else if (temp > 30 && temp < 40) {
+        printf("Its Hot\n");
+    } else if (temp >= 40) {
+        printf("Its Very Hot\n");
+    }
+
+    return 0;
+}
 
